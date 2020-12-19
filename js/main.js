d6ffb9c77a5e1305b20e0272204fe686b0e5e43c
Andrew <feshykandrew.work@gmail.com>
2020-12-19 18:30:41 +0200

    header adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-
+    $('.touch-menu').PopupSlider({
+        effect: 'right',
+        button: '.burger',
+        closeButton: '.burger',
+        container: 'header ',
+        onMenuOpen: function () {
+            document.activeElement.blur();
+        },
+        onMenuClose: function () {
+            document.activeElement.blur();
+        }
+    });
 })
